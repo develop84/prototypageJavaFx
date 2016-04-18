8a66eaf23b117320f9c59a488dfabc152fa38fdb
LeVirus <charbonneau_cyril@hotmail.fr>
2016-04-18 17:26:14 +0200

    creation des interfaces.

--- JavaFXTP3/src/m1/piu/FXMLMainFrameController.java
+++ JavaFXTP3/src/m1/piu/FXMLMainFrameController.java
@@ -22,8 +22,11 @@ public class FXMLMainFrameController implements Initializable {
     private Label label;
     
     @FXML
+    Button newContact;
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        
         label.setText("Hello World!");
     }
     
---
+++ JavaFXTP3/src/m1/piu/FXMLNewContacttController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m1.piu;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author cyril
+ */
+public class FXMLNewContacttController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void pressAddress(ActionEvent event){
+        
+    }
+    
+    public void pressPhone(ActionEvent event){
+        
+    }
+    
+    public void pressIdentity(ActionEvent event){
+        
+    }
+    
+}
